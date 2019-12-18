e555e114eb3ea0cb47e2ce7574480143b8b23baa
laura antonius <laurastolen@gmail.com>
2019-12-18 10:04:37 -0800

    added schema

---
+++ schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT,
+  author VARCHAR(255),
+  isbn VARCHAR(255),
+  bookshelf VARCHAR(255),
+  image_url VARCHAR(255)
+);
+
+INSERT INTO books (title, description, author, isbn, bookshelf, image_url) 
+VALUES('Book', 'a really good book', 'Laura', '11122', 'fiction', 'https://images.pexels.com/photos/1005324/literature-book-open-pages-1005324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
+
+INSERT INTO books (title, description, author, isbn, bookshelf, image_url) 
+VALUES('Book2', 'a really good book2', 'Kai', '11123', 'non-fiction', 'https://images.pexels.com/photos/1005324/literature-book-open-pages-1005324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
